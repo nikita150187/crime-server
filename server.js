6ee8844f4b45696659bd3a38cd93ac2ee31060f2
nikita150187 <nikita.taware@stud.sundsgarden.se>
2025-04-08 14:33:18 +0530

    Create route for fetching crime locations with headlines

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import axios from "axios";
 import swaggerUi from 'swagger-ui-express';
 import yaml from 'js-yaml';
+import fs from 'fs';
+
 const app = express();
 const PORT = 3000;
 
@@ -17,10 +19,25 @@ app.get("/crimes", async(req, res) => {
             "https://brottsplatskartan.se/api/events/?location=helsingborg"
         );
         res.json(response.data.data[0]);
-        console.log(response.data.data[0]);
+        //console.log(response.data.data[0]);
         
     } catch (err) {
         console.error(err);
+        res.status(500).json({message:`Error fetching crimes`});
+    }
+});
+
+app.get("/crimes/locations", async(req, res) => {
+    try {
+        const response = await axios.get(
+            "https://brottsplatskartan.se/api/events/?location=helsingborg&limit=5"
+           );
+           console.log(response.data);
+           const locations = response.data.data.map(crime => crime.headline);
+        res.json(locations);  
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: `Error fetching crime locations`});
     }
 });
 
